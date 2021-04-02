99830f674da930f522f405235b06182e2c986bcb
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2021-04-03 02:03:48 +0900

    줄 세우기

---
+++ algos_assignment/3rd_grade/SongHyeMin_songfox00/week1/2252.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+vector<int>v[100001];
+int ind[100001] = { 0, };
+int n, m;
+queue<int>q;
+
+void bfs() {
+	for (int i = 1; i <= n; i++) {
+		if (ind[i] == 0)
+			q.push(i);
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int next = q.front();	
+		q.pop();
+
+		cout << next << ' ';
+
+		for (int j = 0; j < v[next].size(); j++) {
+			int nx = v[next][j];
+			ind[nx]--;
+			if (ind[nx] == 0)	//in degree==0
+				q.push(nx);
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+	
+	cin >> n >> m;
+
+	for (int i = 1; i <= m; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		ind[b]++;
+	}
+	bfs();
+
+	return 0;
+}
