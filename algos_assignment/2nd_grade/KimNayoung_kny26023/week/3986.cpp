4f79fb2a000e388dd8621d9a502a1fe5f98a09be
kny26023 <81564554+kny26023@users.noreply.github.com>
2021-04-05 17:19:15 +0900

    Create 3986.cpp

---
+++ algos_assignment/2nd_grade/KimNayoung_kny26023/week/3986.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+int main() {
+	int n;
+	int cnt = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		stack<char> s;
+		string temp;
+		cin >> temp;
+		for (int j = 0; j < temp.length(); j++) {
+			if (s.size() != 0) {
+				if (s.top() == temp[j]) {
+					s.pop();
+				}
+				else {
+					s.push(temp[j]);
+				}
+			}
+			else {
+				s.push(temp[j]);
+			}
+		}
+		if (s.size() == 0) {
+			cnt++;
+		}
+	}
+	printf("%d", cnt);
+}
