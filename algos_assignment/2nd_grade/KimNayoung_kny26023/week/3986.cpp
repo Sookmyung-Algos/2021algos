4051b55d7fad40369263fdbc8be636e2f128c16a
kny26023 <81564554+kny26023@users.noreply.github.com>
2021-04-05 17:32:31 +0900

    Update 3986.cpp

--- algos_assignment/2nd_grade/KimNayoung_kny26023/week/3986.cpp
+++ algos_assignment/2nd_grade/KimNayoung_kny26023/week/3986.cpp
@@ -5,27 +5,26 @@
 using namespace std;
 
 int main() {
-	int n;
-	int cnt = 0;
+	int n, cnt = 0;
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		stack<char> s;
-		string temp;
+		stack<char> s;	//stack s 만들어주기
+		string temp;	//cin으로 받을 변수 만들어주기
 		cin >> temp;
 		for (int j = 0; j < temp.length(); j++) {
-			if (s.size() != 0) {
-				if (s.top() == temp[j]) {
-					s.pop();
+			if (s.size() != 0) {	//s가 비어있지 않을 때
+				if (s.top() == temp[j]) {	//s.top()이 temp[j]와 같을 때
+					s.pop();	
 				}
-				else {
+				else {	//s.top()이 temp[j]와 다를 때
 					s.push(temp[j]);
 				}
 			}
-			else {
+			else {	//s가 비어있을 때
 				s.push(temp[j]);
 			}
 		}
-		if (s.size() == 0) {
+		if (s.size() == 0) {	//s.size()가 0이면 모두 짝이 지어졌다는 뜻
 			cnt++;
 		}
 	}
