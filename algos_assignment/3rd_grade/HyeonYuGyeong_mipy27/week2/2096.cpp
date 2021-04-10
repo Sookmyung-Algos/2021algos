48d0cab344e3be68d590b436767924f15cfc1ef3
noeyh <72396865+yugyeongh@users.noreply.github.com>
2021-04-10 19:09:14 +0900

    Create 2096.cpp

---
+++ algos_assignment/3rd_grade/HyeonYuGyeong_mipy27/week2/2096.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr_max[3]={0},arr_min[3]={0};
+    int temp_max[3]={0},temp_min[3]={0};
+    int n;
+    
+    cin >> n;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<3;j++){
+            cin >> temp_max[j];
+            temp_min[j] = temp_max[j];
+            temp_max[j] += max(arr_max[1],(j==1) ? max(arr_max[0],arr_max[2]):arr_max[j]);
+            temp_min[j] += min(arr_min[1],(j==1) ? min(arr_min[0],arr_min[2]):arr_min[j]);
+        }
+        memcpy(arr_max,temp_max,sizeof(int)*3);
+        memcpy(arr_min,temp_min,sizeof(int)*3);
+    }
+    sort(arr_max,arr_max+3);
+    sort(arr_min,arr_min+3);
+    cout << arr_max[2] << " " <<arr_min[0] <<'\n';
+    return 0;
+}
