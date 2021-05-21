a6eb6c7da7df7c5061d9ee6e914e3a3c67160347
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2021-05-22 00:38:19 +0900

    최소 힙

---
+++ algos_assignment/3rd_grade/SongHyeMin_songfox00/week4/1927.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>>pq;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL), cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	while (n--) {
+		int a;
+		cin >> a;
+
+		if (!a && pq.empty()) {
+			cout << 0 << '\n';
+		}
+		else if (!a) {
+			cout << pq.top() << '\n';
+			pq.pop();
+		}
+		else {
+			pq.push(a);
+		}
+	}
+	return 0;
+}
