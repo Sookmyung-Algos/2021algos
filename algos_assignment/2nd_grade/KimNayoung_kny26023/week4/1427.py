a1b84fd6d9faf25936ccab385616a669c385eaba
kny26023 <81564554+kny26023@users.noreply.github.com>
2021-05-26 13:26:29 +0900

    Update 1427.py

--- algos_assignment/2nd_grade/KimNayoung_kny26023/week4/1427.py
+++ algos_assignment/2nd_grade/KimNayoung_kny26023/week4/1427.py
@@ -2,8 +2,9 @@ nums = input()
 nums = [int(n)  for n in nums]
 #숫자들을 int형으로 바꿔 nums 배열을 만든다.
 
-ordered_nums = sorted(nums, reverse=True)
+order = sorted(nums, reverse=True)
 #ordered_nums에 sorted(nums, reverse=True)를 넣어준다. 내림차순이기 때문에 그러하다.
 
-for n in ordered_nums : 
+for n in order : 
     print(n, end="")
+https://github.com/kny26023/2021algos/blob/patch-27/algos_assignment/2nd_grade/KimNayoung_kny26023/week4/1427.py
