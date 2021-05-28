8a7e190983b204621781ce72b704341429294144
dbsdlqls <49243877+dbsdlqls@users.noreply.github.com>
2021-05-28 23:05:07 +0900

    Create 11726.cpp

---
+++ algos_assignment/3rd_grade/YunYiBin_yibin99/week5/11726.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int dp[1001]={0, };
+
+int main(){
+    int n;
+    cin >> n;
+    dp[1]=1; dp[2]=2;
+    for(int i=3;i<=n;i++){
+        dp[i]=(dp[i-1]+dp[i-2])%10007;
+    }
+    cout << dp[n];
+}
