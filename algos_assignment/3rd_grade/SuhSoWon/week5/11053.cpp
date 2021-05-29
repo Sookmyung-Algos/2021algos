561c441c24f66f27ec4f44866b10fb26db762ea6
PleaseErwin <79669001+PleaseErwin@users.noreply.github.com>
2021-05-29 18:24:14 +0900

    Create 11053.cpp

---
+++ algos_assignment/3rd_grade/SuhSoWon/week5/11053.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int numA[1002];
+int dp[1002];
+int main() {
+	int n, maxlen = 0, cnt;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> numA[i];
+	}
+	for (int j = 0; j < n; j++) {
+		dp[j] = 1;
+		for (int k = 0; k < j; k++) {
+			if (numA[j] > numA[k])
+				dp[j] = max(dp[j], dp[k] + 1);
+		}
+		if (dp[j] > maxlen)
+			maxlen = dp[j];
+	}
+	cout << maxlen << '\n';
+}
