bc9aebd67196da93e49d5f5378489dce80a83f15
gaeunlee <62925109+0923ule@users.noreply.github.com>
2021-06-26 22:24:51 +0900

    Update l2x3ge_4796.cpp

--- algos_semina/2nd_seminar/seminar_1/A_4796/l2x3ge_4796.cpp
+++ algos_semina/2nd_seminar/seminar_1/A_4796/l2x3ge_4796.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
 
+using namespace std;
+
+int main() {
+	int L, P, V, idx = 1, result;
+	cin >> L >> P >> V;
+	while ((L != 0) && (P != 0) && (V != 0)) {
+		result = (V / P)*L;
+		if (V%P > L)
+			result += L;
+		else
+			result += V % P;
+		cout << "Case " << idx << ": " << result << "\n";
+		idx++;
+		cin >> L >> P >> V;
+	}
+	return 0;
+}
