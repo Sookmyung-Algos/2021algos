c48f04f1d4c357f27a0d209f7955b29459f231c8
gaeunlee <62925109+0923ule@users.noreply.github.com>
2021-06-26 22:21:17 +0900

    Update l2x3ge_5545.cpp

--- algos_semina/2nd_seminar/seminar_1/E_5545/l2x3ge_5545.cpp
+++ algos_semina/2nd_seminar/seminar_1/E_5545/l2x3ge_5545.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+	int n, a, b, c, cal = 0, price = 0;
+	int d[10001];
+	cin >> n >> a >> b >> c;
+	for (int i = 0;i < n;i++) {
+		cin >> d[i];
+	}
+	sort(d, d + n, greater<>());
+	cal += c;
+	price += a;
+	int result = cal / price;
+	for (int i = 0;i < n;i++) {
+		cal += d[i];
+		price += b;
+		if (result > cal / price)
+			break;
+		result = cal / price;
+	}
 
+	cout << result;
+	return 0;
+}
