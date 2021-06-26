f9496ab81759f784f80fdfb5ef87f6bc8104ca0e
metamong-Hi <55707601+metamong-Hi@users.noreply.github.com>
2021-06-26 22:03:18 +0900

    Create 2839.cpp

---
+++ algos_assignment/3rd_grade/seojinlee_metamong/week6/2839.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using  namespace std;
+int main()
+{
+
+	int n;
+	int cnt=0;
+	cin>>n;
+	while(1) {
+		if(n%5==0) {
+			cnt+=n/5;
+			cout<<cnt;
+			break;
+		}
+		n-=3;
+		cnt++;
+		if (n<0) {
+			cout<<"-1";
+			break;
+		}
+
+	}
+}
