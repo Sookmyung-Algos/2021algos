996cec2f79d7034305804457cec325ef22cebed5
metamong-Hi <55707601+metamong-Hi@users.noreply.github.com>
2021-08-15 23:53:40 +0900

    Create 9372.cpp

---
+++ algos_assignment/3rd_grade/seojinlee_metamong/week7/9372.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	while(n--){
+		int a,b;
+		cin>>a>>b;
+		for(int i=0;i<b;i++){
+			int c,d;
+			cin>>c>>d;
+		}
+		cout<<a-1<<"\n";
+	}
+}
