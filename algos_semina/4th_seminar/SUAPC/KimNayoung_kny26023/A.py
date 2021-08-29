8f003f1cb61fce38acad40b393c910f22146620f
kny26023 <81564554+kny26023@users.noreply.github.com>
2021-08-29 15:57:33 +0900

    Create A.py

---
+++ algos_semina/4th_seminar/SUAPC/KimNayoung_kny26023/A.py
@@ -0,0 +1,24 @@
+from sys import stdin
+from collections import deque
+input= stdin.readline
+
+n, k= map(int, input().rstrip().split())
+v= list(map(int, input().rstrip().split()))
+
+v.sort()
+result=k
+maxV=0
+for i in range(n-1):
+    a= v[0]*(i+1)
+    b=v[i+1]*(n-i-1)
+    if(a+b>maxV):
+        maxV=a+b
+    '''if(result>=k/(a+b)):
+        result=k/(a+b)
+
+if(result%1!=0):
+    result+=1'''
+result=k//maxV
+if(k%maxV!=0):
+    result+=1
+print(int(result))
