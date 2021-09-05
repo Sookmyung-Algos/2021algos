2428158ada5d06cb6ac4d178246a31ad948f5e3e
Hyemin Song <69200763+songfox00@users.noreply.github.com>
2021-09-05 11:20:30 +0900

    휴먼 파이프라인

---
+++ algos_semina/4th_seminar/SUAPC/SongHyeMin_songfox00/22981.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(NULL);
+	cin.tie(0), cout.tie(0);
+
+	int n;
+	vector<unsigned long long> arr;
+	unsigned long long k;
+
+	cin >> n;
+	cin >> k;
+	for (int i = 0; i < n; i++) {
+		unsigned long long t;
+		cin >> t;
+		arr.push_back(t);
+	}
+	sort(arr.begin(), arr.end());
+	unsigned long long num = arr[0] * 1 + arr[1] * (n - 1);
+	for (int i = n - 2; i >= 1; i--) {
+		num = ((num >(arr[0] * (n - i) + arr[n - i] * i)) ? num : (arr[0] * (n - i) + arr[n - i] * i));
+	}
+	
+	if (k % num == 0) cout << k / num;
+	else cout << (k / num) + 1;
+}
