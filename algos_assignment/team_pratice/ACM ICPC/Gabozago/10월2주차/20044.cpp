7346d101650430c5013f4303618e40da518373a2
Yena Park <parkyena01@sookmyung.ac.kr>
2021-10-10 16:57:21 +0900

    Create 20044.cpp

---
+++ algos_assignment/team_pratice/ACM ICPC/Gabozago/10월2주차/20044.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int n, w, ans = 200001;
+	vector<int> v;
+
+	cin >> n;
+	for (int i = 0; i < 2 * n; i++) {
+		cin >> w;
+		v.push_back(w);
+	}
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < 2 * n; i++) {
+		ans = min(ans, v[i] + v[2 * n - 1 - i]);
+	}
+	cout << ans;
+}
