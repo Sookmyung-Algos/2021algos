b64acd3a181a1679817ffca456624fd8d220aa74
whaeun25 <61380136+whaeundo25@users.noreply.github.com>
2021-10-11 01:28:30 +0900

    Create 11657.cpp

---
+++ algos_assignment/team_pratice/ACM ICPC/Algospecial/10월 2주차/11657.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#include<vector>
+#include<cmath>
+
+#define INF 1e9
+
+using namespace std;
+
+int n, m;
+long long dist[510];
+
+vector<pair<pair<int, int>, int>> edge;
+
+int main(){
+    
+    cin >> n >> m;
+    
+    for(int i = 1; i <= n; i++){
+        dist[i] = INF;
+    }
+    
+    for(int i = 0; i< m; i++){
+        int a, b, c;
+        
+        cin >> a >> b >> c;
+        
+        edge.push_back({{a, b}, c});
+    }
+    
+    //1번 도시에서 출발하므로 dist[1] = 0이다.
+    dist[1] = 0;
+    
+    for(int i = 1; i < n; i++){
+        for(int j = 0 ; j< edge.size(); j++){
+            int a = edge[j].first.first;
+            int b = edge[j].first.second;
+            int cost = edge[j].second;
+            
+            if(dist[a] == INF){
+                continue;
+            }
+            
+            if(dist[b] > dist[a] + cost){
+                dist[b] = dist[a] + cost;
+            }
+        }
+    }
+    
+    for(int i =0; i< edge.size(); i++){
+        int a = edge[i].first.first;
+        int b = edge[i].first.second;
+        int cost = edge[i].second;
+        
+        if(dist[a] == INF){
+            continue;
+        }
+        
+        if(dist[b] > dist[a] + cost){
+            cout << -1 <<"\n";
+            return 0;
+        }
+    }
+    
+    for(int i = 2; i <= n; i++){
+        if(dist[i] == INF){
+            cout << -1 << "\n";
+        }
+        else{
+            cout << dist[i] << "\n";
+        }
+    }
+    
+    
+}
