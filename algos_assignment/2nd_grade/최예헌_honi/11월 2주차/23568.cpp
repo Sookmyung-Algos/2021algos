c2199e70cd5afb86c25c0e54ac3d01b89518f4a7
honi20 <81567670+honi20@users.noreply.github.com>
2021-11-14 21:47:55 +0900

    Create 23568.cpp

---
+++ algos_assignment/2nd_grade/최예헌_honi/11월 2주차/23568.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int me;
+int home;
+vector <pair<int, int>> v;
+
+int main() {
+
+   cin >> n;
+   for (int i = 0; i < n; i++) {
+      int a, c;
+      char b;
+      cin >> a >> b >> c;
+
+      int start = a;
+      int end;
+      if (b == 'L') {
+         end = a - c;
+      }
+      else if (b == 'R') {
+         end = a + c;
+      }
+      v.push_back(make_pair(start, end));
+   }
+
+   cin >> me; // 현재 내위치 0
+
+   int move = 1;
+   while (move <= n) {
+      for (int i = 0; i < n; i++) {
+         if (me == v[i].first){
+            me = v[i].second;
+            break;
+         }
+      }
+      if (move == n)cout << me;
+      move++;
+   }
+
+   return 0;
+}
