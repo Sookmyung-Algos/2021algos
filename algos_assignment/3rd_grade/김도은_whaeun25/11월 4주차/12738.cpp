4b6fdb32213bded2d185af4bcff0779d939c5482
whaeun25 <61380136+whaeundo25@users.noreply.github.com>
2021-11-23 23:02:40 +0900

    Create 12738.cpp

---
+++ algos_assignment/3rd_grade/김도은_whaeun25/11월 4주차/12738.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> v;
+
+int main() {
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++){
+        int cost;
+        cin >> cost;
+        
+        if(v.empty() || v.back() < cost){
+            v.push_back(cost);
+        }
+        else{
+            int index = lower_bound(v.begin(), v.end(), cost) - v.begin();
+            v[index] = cost;
+        }
+    }
+    cout << v.size() << "\n";
+    
+}
