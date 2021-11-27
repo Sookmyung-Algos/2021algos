94f02699a994067121a0ca648bd12139b75aa1cf
YeongjuJeong <81597993+YeongjuJeong@users.noreply.github.com>
2021-11-27 19:52:45 +0900

    Create 1275.cpp

---
+++ algos_assignment/2nd_grade/정영주_yeongjujeong1021/11월 4주차/1275.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long* tree;
+long long arr[1000001];
+
+long long init(int node, int s, int e) {
+	if (s == e) return tree[node] = arr[s];
+	long long m = (s + e) / 2;
+	return tree[node] = init(node * 2, s, m) + init(node * 2 + 1, m + 1, e);
+}
+
+void update(int node, int s, int e, int idx, long long dif) {
+	if (idx < s || idx > e) return;
+	tree[node] += dif;
+	if (s == e) return;
+	int m = (s + e) / 2;
+	update(node * 2, s, m, idx, dif);
+	update(node * 2 + 1, m + 1, e, idx, dif);
+}
+
+long long sum(int node, int s, int e, int l, int r) {
+	if (l > e || r < s) return 0;
+	if (l <= s && e <= r) return tree[node];
+	int m = (s + e) / 2;
+	return sum(node * 2, s, m, l, r)
+		+ sum(node * 2 + 1, m + 1, e, l, r);
+}
+
+int main() {
+	cin.tie(NULL); cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n, q;
+	cin >> n >> q;
+
+	int height = (int)ceil(log2(n));
+	tree = new long long[1 << (height + 1)];
+
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	init(1, 0, n - 1);
+
+	for (int i = 0, x, y, a, b; i < q; i++) {
+		cin >> x >> y >> a >> b;
+		if (x > y) swap(x, y);
+		cout << sum(1, 0, n - 1, x - 1, y - 1) << '\n';
+		update(1, 0, n - 1, a - 1, b - arr[a - 1]);
+		arr[a - 1] = b;
+	}
+
+	return 0;
+}
